ab9fb9374b816006064c4d763f88e19760d3668e
Rashad-Stack <tex.eng.rashed@gmail.com>
2023-11-26 02:25:49 +0600

    Add bcryptjs for password hashing

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,6 +4,7 @@ import {
   InternalServerErrorException,
 } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
+import * as bcrypt from "bcryptjs";
 import { Repository } from "typeorm";
 import { AuthCredentialsDto } from "./dto/auth-credentials.dto";
 import { User } from "./user.entity";
@@ -20,9 +21,9 @@ export class AuthService {
     try {
       const user = new User();
       user.username = username;
-      user.password = password;
-
-      await user.save();
+      user.password = await this.hashPassword(password);
+      console.log(user.password);
+      // await user.save();
     } catch (error) {
       if (error.code === "23505") {
         throw new ConflictException("Username already exists");
@@ -30,5 +31,10 @@ export class AuthService {
         throw new InternalServerErrorException();
       }
     }
+  }
+
+  private async hashPassword(password: string): Promise<string> {
+    const salt = await bcrypt.genSalt();
+    return bcrypt.hash(password, salt);
   }
 }
