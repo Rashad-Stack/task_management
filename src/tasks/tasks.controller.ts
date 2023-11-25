7652827d42c264d59d80f32f75676a490ee87d1d
Rashad-Stack <tex.eng.rashed@gmail.com>
2023-11-25 14:54:14 +0600

    Add query pram validator

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -23,7 +23,7 @@ export class TasksController {
   }
 
   @Get()
-  getTasks(@Query() filterDto: getTaskFilterDto): Task[] {
+  getTasks(@Query(ValidationPipe) filterDto: getTaskFilterDto): Task[] {
     if (Object.keys(filterDto).length) {
       return this.tasksService.getTasksWithFilters(filterDto);
     }
