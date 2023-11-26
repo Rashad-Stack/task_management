7418a70e1d400065ac86fcb8c8d53dcc2f80dd90
Rashad-Stack <tex.eng.rashed@gmail.com>
2023-11-26 22:04:32 +0600

    Add CORS configuration for local development

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ async function bootstrap() {
   const logger = new Logger("bootstrap");
   const port = process.env.PORT || serverConfig.port;
   const app = await NestFactory.create(AppModule);
+  app.enableCors({ origin: serverConfig.origin });
 
   await app.listen(port);
   logger.log("Application listening on port " + port);
